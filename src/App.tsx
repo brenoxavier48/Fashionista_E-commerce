d63cfc18db77741bdb705cc2447f3a149e32d899
Breno Henrique Xavier <brenoxavier8.bx@gmail.com>
2020-12-28 21:57:46 -0300

    feat: init header

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, MouseEvent, useEffect } from "react";
 import { CountButton, MainButton, SizeButton, HeaderButton } from "./components/ui/buttons";
 import { InputSearch } from "./components/ui/inputs";
 import Counter from './components/Counter'
+import Header from './components/Header'
 import { ProductService } from './services/Product'
 
 function App() {
@@ -17,35 +18,7 @@ function App() {
   },[])
   return (
     <div className="App">
-      <div>
-        <CountButton></CountButton>
-        <CountButton decrease></CountButton>
-        <MainButton label="Adicionar à Sacola"></MainButton>
-        <div style={{width:"290px", padding:"10px"}}>
-          <MainButton label="Adicionar à Sacola"></MainButton>
-        </div>
-        <SizeButton size="PP" available selected></SizeButton>
-        <SizeButton size="P" available={false} selected></SizeButton>
-        <SizeButton size="M" available></SizeButton>
-        <SizeButton size="G" available></SizeButton>
-        <SizeButton size="G" available={false}></SizeButton>
-        <HeaderButton icon="search"></HeaderButton>
-        <HeaderButton icon="shoppingCart"></HeaderButton>
-        <HeaderButton icon="shoppingCart" itemsQuantity={233}></HeaderButton>
-        <InputSearch></InputSearch>
-        <div style={{width:"290px", padding:"10px"}}>
-          <InputSearch></InputSearch>
-        </div>
-        <Counter
-          value={value}
-          decrease={(event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
-            setValue(currentValue => currentValue - 1)
-          }}
-          increase={(event: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
-            setValue(currentValue => currentValue + 1)
-          }}
-        ></Counter>
-      </div>
+      <Header/>
     </div>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectAllProducts } from '../../store/Products/products.selectors'
+import Header from '../../components/Header'
 
 
 
-export default () => {
+const Home = () => {
 
   
-
   return (
-    <div>
+    <div className="home-container">
+      <Header></Header>
 
     </div>
   )

24d80ab03bfb8dc75abaf2ed73a449497fefe5d9
Breno Henrique Xavier <brenoxavier8.bx@gmail.com>
2020-12-30 22:42:47 -0300

    feat: make Home page request catalog and add it in store

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { selectAllProducts } from '../../store/Products/products.selectors'
+import { useDispatch } from 'react-redux'
+import { addCatalog } from '../../store/Products/products.actions'
+
 import { ProductService } from '../../services/Product'
 import Header from '../../components/Header'
 import Drawer, { DrawerRules, DrawerType } from '../../components/Drawer'
@@ -10,10 +11,20 @@ import ProductCard from '../../components/ProductCard'
 
 const Home = () => {
 
+  const dispatch = useDispatch()
+
   const [ drawer, setDrawer ] = useState<DrawerRules>({
     isOpen: false,
     type: ''
   })
+
+  useEffect( () => {
+    const productService = new ProductService()
+
+    productService.getCatolog().then((response) => {
+      dispatch(addCatalog(response))
+    })
+  },[])
 
   const makeDrawerObject = (isOpen: boolean, type: DrawerType): DrawerRules => Object.assign({ isOpen, type })
 

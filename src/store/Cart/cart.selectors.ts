20cf8a1ba1a5c13efd2a6a4679e13b05a9a7166c
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-01-09 21:52:12 -0300

    feat: add cart selector to total price and items

--- src/store/Cart/cart.selectors.ts
+++ src/store/Cart/cart.selectors.ts
@@ -1,1 +1,5 @@
 export const selectQuantityProductsCart = (store: any) => store.Cart.itemsQuantity
+
+export const selectTotalPriceProductsCart = (store: any) => store.Cart.totalPrice
+
+export const selectAllProductsCart = (store: any) => store.Cart.items

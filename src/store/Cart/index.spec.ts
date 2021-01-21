e3a886facaa013583deb1a388782035c72f5a479
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-01-20 22:06:00 -0300

    test: Ensuru getTotalPriceAndQuantity return the right quantity

--- src/store/Cart/index.spec.ts
+++ src/store/Cart/index.spec.ts
@@ -13,20 +13,20 @@ import {
 import { getTotalPriceAndQuantity } from './helpers'
 import cartReducer from './cart.reducer'
 
-const makeProductCart = (sku: string): ProductCart => ({
+const makeProductCart = (sku: string, quantity = 1, price = 1): ProductCart => ({
   name: `test-${sku}`,
-  quantity: 1,
-  actual_price: 'R$ 1,00',
+  quantity,
+  actual_price: `R$ ${price},00`,
   installments: 'test',
   image: 'test',
   size: 'test',
   sku
 })
 
-const makeProducts = (quantity: number): ProductCart[] => {
+const makeProducts = (quantity: number, quantityOfEachProduct = 1, priceOfEachProduct = 1): ProductCart[] => {
   let items: ProductCart[] = []
   for (let i = 0; i < quantity; i++) {
-    items.push(makeProductCart(String(i)))
+    items.push(makeProductCart(String(i), quantityOfEachProduct, priceOfEachProduct))
   }
   return items
 }
@@ -41,12 +41,29 @@ const makeInitialState = (initialQuantity: number): CartState => {
   })
 }
 
-describe('Helpers test cases', () => {
+describe('Cart helpers test cases', () => {
   describe('getTotalPriceAndQuantity test cases', () => {
     test('Should return 0 for totalPrice and itemsQuantity if an empty array is passed', () => {
       const { itemsQuantity, totalPrice } = getTotalPriceAndQuantity([])
       expect(itemsQuantity).toBe(0)
-      expect(itemsQuantity).toBe(0)
+      expect(totalPrice).toBe(0)
+    })
+
+    test('Should return the right quantity', () => {
+      const QUANTITY_OF_EACH_ITEM_01 = 1
+      const items01 = makeProducts(5, QUANTITY_OF_EACH_ITEM_01)
+      const TOTAL_QUANTITY_01 = items01.length * QUANTITY_OF_EACH_ITEM_01
+
+      const { itemsQuantity: itemsQuantity01 } = getTotalPriceAndQuantity(items01)
+      
+      const QUANTITY_OF_EACH_ITEM_02 = 3
+      const items02 = makeProducts(5, QUANTITY_OF_EACH_ITEM_02)
+      const TOTAL_QUANTITY_02 = items02.length * QUANTITY_OF_EACH_ITEM_02
+      
+      const { itemsQuantity: itemsQuantity02 } = getTotalPriceAndQuantity(items02)
+      
+      expect(itemsQuantity01).toBe(TOTAL_QUANTITY_01)
+      expect(itemsQuantity02).toBe(TOTAL_QUANTITY_02)
     })
   })
 })

e20348e889b6cc9c304f4e5ec2ee87ec7fe63e68
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-02-11 20:30:26 -0300

    test: Ensure CartProductCard shows the right quantity

---
+++ src/presentation/components/CartProductCard/index.spec.ts
@@ -0,0 +1,22 @@
+import CartProductCard from './'
+import { fireEvent, render } from '@testing-library/react'
+import { renderWithProvider, renderWithProviderWithInitialState } from '../../test/renderHelpers'
+import { storeWithCartInitialState, mockProducts, mockSingleProduct } from '../../test/storeHelpers'
+
+describe('<CartProductCard />', () => {
+
+  const valueTestId = 'counter-value'
+  const increaseButtonTestId = 'counter-increase-button'
+  const decreaseButtonTestId = 'counter-decrease-button'
+
+  test('Ensure CartProductCard shows the right quantity', () => {
+    const SKU = 'test'
+    const QUANTITY = 1
+    const products = [ mockSingleProduct(SKU, QUANTITY) ]
+    const store = storeWithCartInitialState([ ...products ])
+    const renderWithInitialState = renderWithProviderWithInitialState(store)
+    const { getByTestId } = render(renderWithInitialState(CartProductCard, { product: products[0] }))
+    const value = getByTestId(valueTestId)
+    expect(value.innerHTML).toBe(String(QUANTITY))
+  })
+})

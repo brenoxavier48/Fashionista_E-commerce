4e697181a183aeee939ebb9f98e3e384340062a8
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-02-11 20:33:29 -0300

    feat: make selector for single product cart

--- src/store/Cart/cart.selectors.ts
+++ src/store/Cart/cart.selectors.ts
@@ -5,3 +5,5 @@ export const selectQuantityProductsCart = (store: RootState) => store.Cart.items
 export const selectTotalPriceProductsCart = (store: RootState) => store.Cart.totalPrice
 
 export const selectAllProductsCart = (store: RootState) => store.Cart.items
+
+export const selectSingleProductCart = (sku: string) =>  (store: RootState) => store.Cart.items.find(product => product.sku === sku)

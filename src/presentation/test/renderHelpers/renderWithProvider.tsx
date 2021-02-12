df4d87d93d17b3bd015ed6763311f860e9619dd2
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-02-12 18:37:42 -0300

    reafctor: make props argument for renderWithProvider optional

--- src/presentation/test/renderHelpers/renderWithProvider.tsx
+++ src/presentation/test/renderHelpers/renderWithProvider.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from '../../../store'
 
-export const renderWithProvider = (Component: React.FC<any>, props: any) => (
+export const renderWithProvider = (Component: React.FC<any>, props?: any) => (
   <Provider store={store}>
     <Component {...props}></Component>
   </Provider>
 )
 
 export const renderWithProviderWithInitialState = (intialStore: typeof store) => (
-  (Component: React.FC<any>, props: any) => (
+  (Component: React.FC<any>, props?: any) => (
     <Provider store={intialStore}>
       <Component {...props}></Component>
     </Provider>

babb65391be05530bd3ac502735a39a7d733990f
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-02-18 00:31:30 -0300

    test: ensure Cart product remove a item correctly

--- src/presentation/components/Cart/index.spec.ts
+++ src/presentation/components/Cart/index.spec.ts
@@ -12,6 +12,7 @@ describe('<Cart />', () => {
   const valueTestId = 'counter-value'
   const increaseButtonTestId = 'counter-increase-button'
   const decreaseButtonTestId = 'counter-decrease-button'
+  const removeItemTestId = 'drawer-product-cart-remove-item'
   const drawerProductTestId = 'drawer-product'
   const cartTotalPriceTestId = 'cart-total-price'
   let storeTest: typeof store
@@ -25,9 +26,8 @@ describe('<Cart />', () => {
     const products = mockProducts(QUANTITY)
     storeTest = storeWithCartInitialState([ ...products ])
     const renderWithInitialState = renderWithProviderWithInitialState(storeTest)
-    const { getAllByTestId, getByTestId } = render(renderWithInitialState(Cart))
+    const { getAllByTestId } = render(renderWithInitialState(Cart))
     const drawerProducts = getAllByTestId(drawerProductTestId)
-    const cartTotalPrice = getByTestId(cartTotalPriceTestId)
     expect(drawerProducts.length).toBe(QUANTITY)
   })
 
@@ -58,5 +58,21 @@ describe('<Cart />', () => {
     const value = getByTestId(valueTestId)
     const currentValue = String(QUANTITY - 1)
     expect(value.innerHTML).toBe(currentValue)
+  })
+
+
+  test('Ensure Cart product remove a item correctly', () => {
+    const QUANTITY = 4
+    const products = mockProducts(QUANTITY)
+    storeTest = storeWithCartInitialState([ ...products ])
+    const renderWithInitialState = renderWithProviderWithInitialState(storeTest)
+    const { getAllByTestId, getAllByText } = render(renderWithInitialState(Cart))
+    const removeItem = getAllByTestId(removeItemTestId)
+    fireEvent.click(getAllByText('Remover item')[0])
+    setTimeout(() => {
+      const drawerProducts = getAllByTestId(drawerProductTestId)
+      const currentQuantityOfDistinctProducts = QUANTITY - 1
+      expect(drawerProducts.length).toBe(currentQuantityOfDistinctProducts)
+    }, 400)
   })
 })

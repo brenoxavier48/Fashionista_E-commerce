786a69bf084a6a5c1fc0a55e9beb0e90c999fdb6
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-02-19 02:33:25 -0300

    test: Ensure SearchProduts doesn't show any item and show 0 items if the search input is empty

--- src/presentation/components/SearchProducts/index.spec.ts
+++ src/presentation/components/SearchProducts/index.spec.ts
@@ -5,18 +5,22 @@ import { storeWithProductsInitialState, mockCatalog, mockProductsInitialState } 
 import { store } from '../../../store'
 import { cleanCart } from '../../../store/Cart/cart.actions'
 
+const makeTotalItemsShouldBe = (quantity: number) => `${quantity} items`
 
 describe('<SearchProducts />', () => {
 
   const drawerProductTestId = 'drawer-product'
   const searchProductItemsSectionTestId = 'search-product-items-section'
+  const searchProductItemsQuantityTestId = 'search-product-items-quantity'
 
   test('Ensure SearchProduts doesn\'t show any item if the search input is empty', () => {
     const store = storeWithProductsInitialState(mockProductsInitialState(5))
     const renderWithProvider = renderWithProviderWithInitialState(store)
     const { getByTestId } = render(renderWithProvider(SearchProducts))
+    const searchProductItemsQuantity = getByTestId(searchProductItemsQuantityTestId)
     const searchProductItemsSection = getByTestId(searchProductItemsSectionTestId)
     const drawerProducts = searchProductItemsSection.getElementsByClassName('drawer-product-container')
     expect(drawerProducts.length).toBe(0)
+    expect(searchProductItemsQuantity.innerHTML).toBe(makeTotalItemsShouldBe(0))
   })
 })

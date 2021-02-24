d6464125cdf72dbf212fecf8aa081dae1c75e662
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2021-02-24 01:09:37 -0300

    test: ensure Header calls handleClickHome function correctly

--- src/presentation/components/Header/index.spec.ts
+++ src/presentation/components/Header/index.spec.ts
@@ -7,41 +7,64 @@ import { storeWithCartInitialState, mockProducts } from '../../test/storeHelpers
 
 describe('<Header/>', () => {
 
+  const homeButtonTestId = 'header-home-button'
   const searchButtonTestId = 'header-search-button'
   const cartButtonTestId = 'header-cart-button'
   const itemsQuantityTestId = 'header-cart-items-quantity'
 
-  test('Should call handleClickSearch function correctly', () => {
+  test('Should call handleClickHome function correctly', () => {
+    const handleClickHome = jest.fn()
     const handleClickSearch = jest.fn()
     const handleClickShoppingCart = jest.fn()
     const { getByTestId } = render(
       renderWithProvider(
         Header, 
-        { handleClickSearch, handleClickShoppingCart }
+        { handleClickSearch, handleClickShoppingCart, handleClickHome }
+      )
+    )
+    const homeButton = getByTestId(homeButtonTestId)  
+    fireEvent.click(homeButton)
+    expect(handleClickHome).toBeCalled()
+    expect(handleClickSearch).not.toBeCalled()
+    expect(handleClickShoppingCart).not.toBeCalled()
+  })
+
+  test('Should call handleClickSearch function correctly', () => {
+    const handleClickHome = jest.fn()
+    const handleClickSearch = jest.fn()
+    const handleClickShoppingCart = jest.fn()
+    const { getByTestId } = render(
+      renderWithProvider(
+        Header, 
+        { handleClickSearch, handleClickShoppingCart, handleClickHome }
       )
     )
     const searchButton = getByTestId(searchButtonTestId)  
     fireEvent.click(searchButton)
+    expect(handleClickHome).not.toBeCalled()
     expect(handleClickSearch).toBeCalled()
     expect(handleClickShoppingCart).not.toBeCalled()
   })
 
   test('Should call handleClickShoppingCart function correctly', () => {
+    const handleClickHome = jest.fn()
     const handleClickSearch = jest.fn()
     const handleClickShoppingCart = jest.fn()
     const { getByTestId } = render(
       renderWithProvider(
         Header, 
-        { handleClickSearch, handleClickShoppingCart }
+        { handleClickSearch, handleClickShoppingCart, handleClickHome }
       )
     )
     const cartButton = getByTestId(cartButtonTestId)  
     fireEvent.click(cartButton)
+    expect(handleClickHome).not.toBeCalled()
     expect(handleClickSearch).not.toBeCalled()
     expect(handleClickShoppingCart).toBeCalled()
   })
 
   test('Shouldn\'t shows the quantity of items if quantity is 0', () => {
+    const handleClickHome = jest.fn()
     const handleClickSearch = jest.fn()
     const handleClickShoppingCart = jest.fn()
     const ITEMS_QUANTITY = 0
@@ -50,7 +73,7 @@ describe('<Header/>', () => {
     const { getByTestId, getAllByTestId } = render(
       renderWithProviderWithInitialState(store)(
         Header, 
-        { handleClickSearch, handleClickShoppingCart }
+        { handleClickSearch, handleClickShoppingCart, handleClickHome }
       )
     )
     const cartButton = getByTestId(cartButtonTestId) 
@@ -59,6 +82,7 @@ describe('<Header/>', () => {
   })
 
   test('Should shows the right quantity of items', () => {
+    const handleClickHome = jest.fn()
     const handleClickSearch = jest.fn()
     const handleClickShoppingCart = jest.fn()
     const ITEMS_QUANTITY = 5
@@ -67,7 +91,7 @@ describe('<Header/>', () => {
     const { getByTestId } = render(
       renderWithProviderWithInitialState(store)(
         Header, 
-        { handleClickSearch, handleClickShoppingCart }
+        { handleClickSearch, handleClickShoppingCart, handleClickHome }
       )
     )
     const itemsQuantity = getByTestId(itemsQuantityTestId)  
